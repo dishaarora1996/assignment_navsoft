88bcb341cbecfde17320b8b31900c39edfaff86a
Disha Arora <disha.arora@navsoft.in>
2023-08-03 19:01:50 +0530

    ingredient api update

--- recipe/serializers.py
+++ recipe/serializers.py
@@ -1,5 +1,30 @@
 
 
+class RecipeSerializer(serializers.ModelSerializer):
+    ingredients = IngredientSerializer(many=True, required=False)
+
+    class Meta:
+        model =[
+            'ingredients'
+        ]
+
+    def _get_or_create_ingredients(self, ingredients, recipe):
+        """Handle getting or ceating ingredients as needed."""
+        auth_user = self.context['request'].user
+        for ingredient in ingredients:
+            ingredient_obj, create = Ingredient.objects.get_or_create(
+                user=auth_user,
+                **ingredient
+            )
+            recipe.ingredients.add(ingredient_obj)
+
+
+    def create(self, **validated_data):
+        ingredients = validated_data.pop('ingredients', [])
+        recipe = Recipe.objects.create(**validated_data)
+        self._get_or_create_ingredients(ingredients, recipe)
+
+
 class IngredientSerializer(serializers.ModelSerializer):
     """Serializers for ingredients."""
 
